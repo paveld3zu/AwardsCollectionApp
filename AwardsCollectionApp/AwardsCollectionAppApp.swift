7dd5a29373a262e9b45b465ae5e3b46d47491d41
paveld3zu <pavel.karunnyj@yandex.ru>
2023-06-27 19:20:36 +0300

    Initial Commit

---
+++ AwardsCollectionApp/AwardsCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionAppApp.swift
+//  AwardsCollectionApp
+//
+//  Created by Pavel Karunnyj on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
